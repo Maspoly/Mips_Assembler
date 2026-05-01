99e77b1f613e757c3c2a813b7fe16f1a9522dba8
AnaHelenaAC <ana.cosme@alunos.ufersa.edu.br>
2026-05-01 12:03:35 -0300

    Add sll function

--- python/mipsR.py
+++ python/mipsR.py
@@ -177,3 +177,23 @@ def mul(dest, src1, src2):
     print(globals()[rd](), end="") 
     print(shamt, end="")
     print(funct)
+
+# ------------------ Função SLL ------------------
+def sll(dest, src1, src2):
+   
+    rd = dest.replace('$', '').replace(',', '')
+    rs = src1.replace('$', '').replace(',', '')
+    rt = src2.replace('$', '').replace(',', '')
+    
+    opcode = 0
+    shamt = 0
+    funct = 0 # Este é o código específico para o sll
+    
+    # Imprimimos sem pular linha, seguindo a ordem: opcode, rs, rt, rd, shamt, funct
+    print(opcode, end="")
+    print(globals()[rs](), end="") 
+    print(globals()[rt](), end="") 
+    print(globals()[rd](), end="") 
+    print(shamt, end="")
+    print(funct)
+
