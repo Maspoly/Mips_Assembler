414017381051b9d68c74fdeddf38a87bd4c3449b
AnaHelenaAC <ana.cosme@alunos.ufersa.edu.br>
2026-05-01 12:16:55 -0300

    Add mflo function

--- python/mipsR.py
+++ python/mipsR.py
@@ -237,7 +237,6 @@ def jr(dest, src1, src2):
 
 # ------------------ Função MFHI ------------------
 def mfhi(dest):
-   
     rd = dest.replace('$', '').replace(',', '')
     
     opcode = 0
@@ -245,6 +244,24 @@ def mfhi(dest):
     rt = 0
     shamt = 0
     funct = 16 # Este é o código específico para o mfhi
+    
+    # Imprimimos sem pular linha, seguindo a ordem: opcode, rs, rt, rd, shamt, funct
+    print(opcode, end="")
+    print(rs, end="") 
+    print(rt, end="")
+    print(globals()[rd](), end="") 
+    print(shamt, end="")
+    print(funct)
+
+# ------------------ Função MFLO ------------------
+def mflo(dest):
+    rd = dest.replace('$', '').replace(',', '')
+    
+    opcode = 0
+    rs = 0
+    rt = 0
+    shamt = 0
+    funct = 18 # Este é o código específico para o mflo
     
     # Imprimimos sem pular linha, seguindo a ordem: opcode, rs, rt, rd, shamt, funct
     print(opcode, end="")
